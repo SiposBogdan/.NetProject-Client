f04c74e6800a424cb7778c551f8a9442fe847ed7
SiposBogdan <bogdan.sipoa@gmail.com>
2025-04-24 13:01:01 +0300

    client-identity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 import { InitService } from './core/services/init.service';
 import { lastValueFrom } from 'rxjs';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 
 
@@ -25,7 +26,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
   provideZoneChangeDetection({ eventCoalescing: true }), 
   provideRouter(routes),
-  provideHttpClient(withInterceptors([errorInterceptor,loadingInterceptor])),
+  provideHttpClient(withInterceptors([
+    errorInterceptor,
+    loadingInterceptor, 
+    authInterceptor])),
   {
     provide: APP_INITIALIZER,
     useFactory: initializeApp,
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,10 +2,13 @@ import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { MatBadge } from '@angular/material/badge';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../features/cart.service';
+import { AccountService } from '../../core/services/account.service';
+import { MatDivider } from '@angular/material/divider';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 
 @Component({
@@ -17,7 +20,12 @@ import { CartService } from '../../features/cart.service';
     MatBadge,
     RouterLink,
     RouterLinkActive,
-    MatProgressBar
+    MatProgressBar,
+    MatDivider,
+    MatMenuItem,
+    MatMenuTrigger,
+    MatMenu
+
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -25,5 +33,17 @@ import { CartService } from '../../features/cart.service';
 export class HeaderComponent {
   busyService = inject (BusyService);
   cartService = inject (CartService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout(){
+    this.accountService.logout().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+
+      }
+    })
+  }
   
 }
